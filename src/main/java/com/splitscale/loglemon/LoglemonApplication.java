f9c0ef50f8674f7203eb4fc6a5407abaf3f198d1
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-04-26 18:22:23 +0800

    spring boot change

---
+++ src/main/java/com/splitscale/loglemon/LoglemonApplication.java
@@ -0,0 +1,13 @@
+package com.splitscale.loglemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoglemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoglemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitscale/loglemon/LoglemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitscale.loglemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoglemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
